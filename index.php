bb3c2c8df1737d188a267ca1313068e22ef54d2b
Sakshi Sinha <128817418+SakshiSinha0901@users.noreply.github.com>
2024-04-07 15:01:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action = "index.php" method = "post">
+        <input type = "checkbox" name = foods[] value = "Pizza">Pizza<br>
+        <input type = "checkbox" name = foods[] value = "Hamburger">Hamburger <br>
+        <input type = "checkbox" name = foods[] value = "Hotdog">Hotdog <br>
+        <input type = "checkbox" name = foods[] value = "Taco">Taco <br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+<?php
+    
+    if (isset($_POST["submit"])) {  // Check if submitted
+      $foods = $_POST["foods"];
+      if (empty($foods)) {  // Check if any checkboxes are selected
+        echo "No food selections made.";
+      } else {
+        echo "You selected: <br>";
+        foreach ($foods as $food) {
+          echo $food . "<br>";
+        }
+      }
+    }
+?>
+
+</html>
